cd85432e27e02d0265184603f49d00e5f467d37f
Weinan Qiu <davidiamyou@gmail.com>
2015-08-20 18:03:53 -0400

    checkpoint 20 merge

---
+++ Alcolator/Alcolator/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  Alcolator
+//
+//  Created by Weinan Qiu on 2015-08-20.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
