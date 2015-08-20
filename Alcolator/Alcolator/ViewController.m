6879419482d7004ec9294024427b3fd0b6836318
Weinan Qiu <davidiamyou@gmail.com>
2015-08-20 17:24:17 -0400

    Added outlets and actions

--- Alcolator/Alcolator/ViewController.m
+++ Alcolator/Alcolator/ViewController.m
@@ -10,6 +10,10 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *beerPercentTextField;
+@property (weak, nonatomic) IBOutlet UISlider *beerCountSlider;
+@property (weak, nonatomic) IBOutlet UILabel *resultLabel;
+
 @end
 
 @implementation ViewController
@@ -22,6 +26,18 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)textFieldDidChange:(UITextField *)sender {
+}
+
+- (IBAction)sliderValueDidChange:(UISlider *)sender {
+}
+
+- (IBAction)buttonPressed:(UIButton *)sender {
+}
+
+- (IBAction)tapGestureDidFire:(UITapGestureRecognizer *)sender {
 }
 
 @end
